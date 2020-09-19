9b83db99e965c77367d5f0493f45e99e0aa31f9c
ezchamp <emile269@gmail.com>
2020-09-19 00:53:57 +0000

    Created ternary operator

--- main.swift
+++ main.swift
@@ -231,6 +231,7 @@ switch currentHR {
 // Remember the currentHR constant already exists and is 147
 
 // Refactor the code below so that largest is declared and assigned to in one line using the ternary operator.
+
 let number1 = 14
 let number2 = 25
 
@@ -240,6 +241,11 @@ if number1 > number2 {
 } else {
     largest = number2
 }
+
+
+largest = number1 > number2 ? number1:number2
+
+print(largest)
 
 // The code below should look similar to code you wrote in the Fitness Decisions exercise. The if-else statement is actually unnecessary, and instead you can print either one statement or the other all on one line using the ternary operator. Go ahead and refactor the code below to do just that.
 
